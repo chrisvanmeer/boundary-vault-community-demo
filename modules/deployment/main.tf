2784a79a0cacb983d05d9bc8ce285699d404838a
Chris van Meer <c.v.meer@atcomputing.nl>
2025-01-03 16:53:33 +0100

    Update nsg to include proxy port

--- modules/deployment/main.tf
+++ modules/deployment/main.tf
@@ -88,7 +88,7 @@ resource "azurerm_network_security_group" "nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "9200-9201"
+    destination_port_range     = "9200-9203"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
